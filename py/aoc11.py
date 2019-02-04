47b2e4cb2a9e80780f76d76e7252f740e53d1df5
Blake Mitchell <bam365@mst.edu>
2019-02-04 14:20:44 -0600

    finish 11 in py

--- py/aoc11.py
+++ py/aoc11.py
@@ -1,5 +1,10 @@
 from collections import namedtuple
+from aoclib import int_stream
+import sys
 
+
+GRID_WIDTH = 300
+GRID_HEIGHT = 300
 
 Point = namedtuple('Point', "x, y")
 
@@ -13,5 +18,35 @@ def cell_power_level(point: Point, serial: int) -> int:
     return hundreds_digit - 5
 
 
+def subsquare_power(point: Point, serial: int) -> int:
+    powers = (cell_power_level(Point(x=x, y=y), serial)
+              for x in range(point.x, point.x + 3)
+              for y in range(point.y, point.y + 3))
+    return sum(powers)
+
+
+def max_subsquare_power(width: int, height: int, serial: int) -> Point:
+    points = (Point(x=x, y=y)
+              for x in range(1, width - 3)
+              for y in range(1, height - 3))
+
+    return max(points, key=lambda point: subsquare_power(point, serial))
+
+
+def main():
+    nums = int_stream(sys.stdin)
+    serial = next(nums)
+    answer = max_subsquare_power(GRID_WIDTH, GRID_HEIGHT, serial)
+    print(f"{answer.x},{answer.y}")
+
+
+if __name__ == '__main__':
+    main()
+
+## TESTS ###################################################
+
 def test_cell_power_level():
     assert cell_power_level(Point(x=3, y=5), 8) == 4
+    assert cell_power_level(Point(x=122, y=79), 57) == -5
+    assert cell_power_level(Point(x=217, y=196), 39) == 0
+    assert cell_power_level(Point(x=101, y=153), 71) == 4
